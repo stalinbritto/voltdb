a73f8f7066f5868e8eeda63def86cc4a34433784
Zeeman He <zhe@voltdb.com>
2020-03-25 17:07:06 -0400

    ENG-19180

--- src/ee/storage/CopyOnWriteContext.cpp
+++ src/ee/storage/CopyOnWriteContext.cpp
@@ -143,11 +143,14 @@ int64_t CopyOnWriteContext::handleStreamMore(TupleOutputStreamProcessor &outputS
                    m_tuplesRemaining = 0;
                 }
                 bool deleteTuple = false;
+                std::ostringstream buf;
+                buf << "SNAP@" << static_cast<const void*>(tuple.address()) << std::endl;
+                LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_WARN, buf.str().c_str());
                 std::ostringstream buffer;
                 buffer << "SNAP: " << tuple.debug(getTable().name()).c_str() << std::endl;
                 LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_WARN, buffer.str().c_str());
                 yield = outputStreams.writeRow(tuple, m_hiddenColumnFilter, &deleteTuple);
-                LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_WARN, ":::SNAP-WRITE:::");
+                LogManager::getThreadLogger(LOGGERID_HOST)->log(LOGLEVEL_WARN, buf.str().c_str());
             }
         }
     }
